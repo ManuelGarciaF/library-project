2af8000b177946485aedbe4e896040e6be85ecbd
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-11 19:37:48 -0300

    initial object tutorial version

---
+++ books.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+Book.prototype.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read ? "already read" : "not read yet"}.`
+}
+
+const animalFarm = new Book("Animal Farm", "George Orwell", 120, true)
+
+console.log(animalFarm.info())
