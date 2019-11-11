ec5aae3a6606d184a9046fa6b69e6a1036a82213
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-11 20:36:00 -0300

    implement render and cleanup

--- books.js
+++ books.js
@@ -1,14 +1,48 @@
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}.`
-    }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
+// Test data
+let books = [];
 
-const animalFarm = new Book("Animal Farm", "George Orwell", 120, true)
+const render = books => {
+  // Make an html element and append it to a parent.
+  const makeElem = (parent, tag, cssClasses) => {
+    const elem = document.createElement(tag);
+    cssClasses.forEach(cssClass => elem.classList.add(cssClass));
+    parent.appendChild(elem);
+    return elem;
+  };
 
-console.log(animalFarm.info())
+  const container = document.querySelector("main .container");
+
+  books.forEach(book => {
+    const card = makeElem(container, "div", ["card", "text-left", "card-body"]);
+
+    const title = makeElem(card, "h5", ["card-title"]);
+    title.innerText = book.title;
+
+    const text = makeElem(card, "p", ["card-text"]);
+    text.innerText = `By ${book.author}, ${book.pages} pages, ${
+      book.read ? "Already read" : "Not read yet"
+    }.`;
+  });
+};
+
+function run() {
+  render(books);
+}
+
+// in case the document is already rendered
+if (document.readyState != "loading") run();
+// modern browsers
+else if (document.addEventListener)
+  document.addEventListener("DOMContentLoaded", run);
+// IE <= 8
+else
+  document.attachEvent("onreadystatechange", function() {
+    if (document.readyState == "complete") run();
+  });
